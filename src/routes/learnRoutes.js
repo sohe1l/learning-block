bf1517f6a2b5a73a6705d9299bdb5dea2c378134
Kriti2885 <42858268+Kriti2885@users.noreply.github.com>
2019-11-07 11:36:21 -0800

    Everything working on console
    Everything is working on console. Need to patch up with front end.

--- src/routes/learnRoutes.js
+++ src/routes/learnRoutes.js
@@ -6,28 +6,26 @@ const projectID = 'learningblocks-258219'
 const translate = new Translate({projectID});
   // http://localhost:8080/learn/spanish/level/1/from/english
   //routes.route('/:target/level/:level/from/:native')
-  routes.route('/languages')
-
-  .get((req, res) => getLearn(req,res));
+routes.route('/languages')
+.get((req, res) => getLearn(req,res));
 
   
-  async function getLearn(req, res){
-    const native = req.params.native;
+async function getLearn(req, res){
+  const native = req.params.native;
     //const level = req.params.level;
     //const native = 'English';
     // add level
-    const result =  await db.query("SELECT * FROM words ORDER BY RAND() LIMIT 0,5");
+  const result =  await db.query("SELECT * FROM words ORDER BY RAND() LIMIT 0,5");
     for(i=0; i<result[0].length;i++){
       const text = result[0][i]['word'];
       const sentence = result[0][i]['sentence'];
+      console.log(text);
       translateText(text, sentence);
-
     }
-  }
+}
 
   async function translateText(text,sentence) {
     const target = 'ru';
-
     console.log(text);
     const [word_translation] = await translate.translate(text, target);
     console.log(`Word Translation: ${word_translation}`);
