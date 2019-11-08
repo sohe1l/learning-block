2813644ffc8b337cf7fcb7fb509ddcc3ed9541a2
Kriti2885 <42858268+Kriti2885@users.noreply.github.com>
2019-11-07 16:42:08 -0800

    rem

--- src/routes/learnRoutes.js
+++ src/routes/learnRoutes.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express');
 const db = require('../model/database.js');
 const routes = express.Router();
@@ -35,8 +36,64 @@ async function getLearn(req, res){
     const target1 = 'en'
     const [converted] = await translate.translate(word_translation, target1);
     console.log(`converted: ${converted}`);
+=======
+import { Router } from 'express';
+import db from '../model/database.js';
+
+const routes = Router();
+
+  // http://localhost:8080/learn/spanish/level/1/from/english
+  //routes.route('/:target/level/:level/from/:native')
+  routes.route('/:target/from/english')
+
+  .get((req, res) => getLearn(req,res));
+
+  
+  async function getLearn(req, res){
+    //const target = req.params.target;
+    //const level = req.params.level;
+    //const native = req.params.native;
+    
+    // add level
+    // const [words, fields] = await db.query("SELECT * FROM words ORDER BY RAND() LIMIT 0,5");
+
+    // translate
+    // Imports the Google Cloud client library
+    const Translate = require('@google-cloud/translate');
+
+    // Your Google Cloud Platform project ID
+    const projectId = 'learningblocks-258219';
+
+    // Instantiates a client
+    const translate = new Translate({
+    projectId: projectId,
+    });
+
+    // The text to translate
+    const text = 'Hello, world!';
+    // The target language
+    const target = 'ru';
+
+    // Translates some text into Russian
+    translate
+      .translate(text, target)
+      .then(results => {
+      const translation = results[0];
+
+      console.log(`Text: ${text}`);
+      console.log(`Translation: ${translation}`);
+      })
+      .catch(err => {
+      console.error('ERROR:', err);
+      });
+
+
+    //var results = [];
+
+    res.render('learn', {translation: translation});
+>>>>>>> Stashed changes
   }
 
     //var results = [];
 
-module.exports = routes;
+export default routes;
