24fa49f4fe3c631676a18e06c75c843cce73d3c0
sohe1l <soheilansari@icloud.com>
2019-11-11 23:34:30 -0800

    Add .env template file.

--- src/model/database.js
+++ src/model/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
  const pool = mysql.createPool({
-    host: '34.83.161.119',
-    user: 'dbuser',
-    password: 'secret',
-    database: 'learningdb',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
