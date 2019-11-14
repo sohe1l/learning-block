423cde6a39a91cd8c9a6882bba554fdfcb933150
Kriti2885 <42858268+Kriti2885@users.noreply.github.com>
2019-11-13 18:44:31 -0800

    learn page working with slideshow

--- src/routes/learnRoutes.js
+++ src/routes/learnRoutes.js
@@ -15,37 +15,43 @@ async function getLearn(req, res){
   var target = req.params.target;
   const level = req.params.level;
   const targetCode = lang_dict_text[target];
-  const userWord = [];
-  const result =  await db.query("SELECT * FROM words where words.level = ? ORDER BY RAND()", [level]);
+  resultset ={};
+  const result =  await db.query("SELECT * FROM words where words.level = ? ORDER BY RAND() limit 0,5", [level]);
     
   for(i=0; i<result[0].length;i++)
     {
       var text = result[0][i]['word'];
       var sentence = result[0][i]['sentence'];
+      var image = result[0][i]['image'];
       if(native!='English')
       {
-        text = translateText(text,'en');
-        sentence = translateText(sentence, 'en');
-        console.log("not native");
+        nativeCode = lang_dict_text[native];
+        trans_text = await translateText(text,nativeCode);
+        trans_sentence = await translateText(sentence, nativeCode);
       };
       
       const translateWord = await translateText(text, targetCode);
       const translateSentence = await translateText(sentence, targetCode);
-      console.log(translateWord);
-      console.log(translateSentence);
+      if(native!='English')
+      {
+        resultset[i] = {'word':trans_text, 'sentence':trans_sentence, 'image':image, 'translatedWord':translateWord, 'translatedSentence':translateSentence};
+      }
+      else{
+        resultset[i] = {'word':text, 'sentence':sentence, 'image':image, 'translatedWord':translateWord, 'translatedSentence':translateSentence};
 
+      }
     };
     
-
   async function translateText(totranslate, target) {
     
     return new Promise((resolve, reject) => {
       googleTranslateLearn.translate(totranslate, target, function (err, translation) {
       return resolve(translation.translatedText);
+       });
     });
-  });
 
   };
+  res.render('learn', {resultset:resultset});
 };
 
 module.exports = routes;
